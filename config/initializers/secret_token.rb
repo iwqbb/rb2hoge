03316f560d4b0e76e33e958e1a3a921852fc366a
iwsk <iwqbb963@kitc.city.kyoto.lg.jp>
2013-12-09 13:11:06 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rb2hoge::Application.config.secret_key_base = '8a684a8c21922cc872f8589282eff8f3e6f2b914be4a8ca444002df3c3e86becfe0bd5a85273ee17ba01745c49762851a6b0746340a8420b9f0d28ade2069d9b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rb2hoge::Application.config.session_store :cookie_store, key: '_rb2hoge_session'
